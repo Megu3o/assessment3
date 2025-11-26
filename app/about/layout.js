5af656067aba8794bae4a4a1608765db87c3124f
Megu3o <20114837@tafe.wa.edu.au>
2025-11-27 03:52:37 +0800

    Remove unused About and NASA API pages

--- app/about/layout.js
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <section className="py-8 md:py-10">
-      <div className="mx-auto max-w-3xl px-4">
-        <main>{children}</main>
-      </div>
-    </section>
-  );
-}
